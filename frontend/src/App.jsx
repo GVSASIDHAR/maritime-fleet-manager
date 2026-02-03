e20042392535672619aa71f9842afde3a48739b3
G.V. Sasidhar <sasidharguturi123@gmail.com>
2026-02-03 19:40:48 +0530

    Fix axios import error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { AuthProvider, useAuth } from './context/AuthContext';
 import Logs from "./pages/Logs";
-
+import axios from 'axios';
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
 
