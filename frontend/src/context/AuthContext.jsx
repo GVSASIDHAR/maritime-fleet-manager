44edc06a4004e0decea4ff5169409549be3c9cd5
G.V. Sasidhar <sasidharguturi123@gmail.com>
2026-02-03 19:53:23 +0530

    hosted without errors

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -21,7 +21,7 @@ export const AuthProvider = ({ children }) => {
 
     const login = async (email, password) => {
         try {
-            const res = await axios.post('http://localhost:5001/api/auth/login', { email, password });
+            const res = await axios.post('/api/auth/login', { email, password });
             const userData = res.data;
 
             // Save data
